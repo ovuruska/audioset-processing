b7fab9d12c1ba214a1876aec4764623c590a770d
Oguz Vuruskaner <ovuruska@outlook.com>
2022-06-17 11:17:30 +0300

    Update before-install.sh

--- before-install.sh
+++ before-install.sh
@@ -1,4 +1,6 @@
 sudo apt-get -y update
 sudo apt-get -y install python3-pip
 sudo apt-get -y install ffmpeg
-sudo apt-get -y install youtube-dl
+
+sudo python3 -m pip install youtube-dl
+hash youtube-dl
